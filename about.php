f3a8b931c1bd740d7d98f4fe6020b1eb77ac686f
arshdeepdgreat <arshdeepdgreat@gmail.com>
2021-10-01 09:47:45 +0530

    task assigned in about page

---
+++ about.php
@@ -0,0 +1,14 @@
+navbar with browse all categories your-panel (signup signin)->buttons 
+use html type write element and display message like hello and welcome to ask bot and chat
+create divs and use unique icons for each of the features
+1.its free 
+    free login and sign up and get access to all the shared knowledge etc
+2.Voice bot
+    android apk to access the information using voice based chatbot anywhere using android device
+    click here to download
+3.Compose
+    You can ask your questions for others to answer and if u feel you can also answer fellow users answers
+4.categories of questions available to browse
+5.start browsing now (add link href)
+
+footer made by who all and copyright symbol
