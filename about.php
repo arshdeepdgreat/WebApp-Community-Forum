988bfd78d6a0a3335a280eaeca379ed8c9e65e21
arshdeepdgreat <arshdeepdgreat@gmail.com>
2021-10-01 09:51:59 +0530

    basic code added

--- about.php
+++ about.php
@@ -1,4 +1,4 @@
-navbar with browse all categories your-panel (signup signin)->buttons 
+<!-- navbar with browse all categories your-panel (signup signin)->buttons 
 use html type write element and display message like hello and welcome to ask bot and chat
 create divs and use unique icons for each of the features
 1.its free 
@@ -11,4 +11,17 @@ create divs and use unique icons for each of the features
 4.categories of questions available to browse
 5.start browsing now (add link href)
 
-footer made by who all and copyright symbol
+footer made by who all and copyright symbol -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About us</title>
+</head>
+<body>
+    
+</body>
+</html>
