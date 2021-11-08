abde6d4ed50543b2c1c60e1e856be3c2230958d6
gurtavrein <70653356+gurtavrein@users.noreply.github.com>
2021-11-08 13:09:13 +0530

    Update qanda_db.sql

--- qanda_db.sql
+++ qanda_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 05, 2021 at 08:01 PM
+-- Generation Time: Nov 08, 2021 at 08:17 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.1
 
@@ -42,10 +42,78 @@ CREATE TABLE `all_users` (
 --
 
 INSERT INTO `all_users` (`user_id`, `username`, `email_id`, `password`, `name`, `DOB`, `dp_image`) VALUES
-(1, 'arshdeepdgreat', 'arshdeepdgreat@gmail.com', '3b900faaf1e564240995ba3e0fa578cb', 'Arshdeep Singh Bhatia', '2002-02-11', 'templates\\images\\arshdeep.png'),
+(1, 'arshdeepdgreat', 'arshdeepdgreat@gmail.com', '3b900faaf1e564240995ba3e0fa578cb', 'Arshdeep Singh', '2002-02-11', 'templates\\images\\arshdeep.png'),
 (5, 'whhtwa', 'arshdeepdgreat@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', 'Arshdeep', '2002-02-11', 'sample'),
 (6, 'gogurleen', 'gurleendgreat@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', 'Gurleen Kaur', '2001-10-03', 'sample'),
-(7, 'arpit', 'arshdeepdgreat@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', 'Arpit', '2002-02-11', 'templates/images/arshdeep dp.png');
+(11, 'goarshdeep', 'arshdeepdgreat@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', 'Arshdeep Singh', '2002-02-11', 'templates/images/arshdeep.png'),
+(19, 'arpit', 'arpit@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', 'Arpit', '2002-02-18', 'templates/images/IMG_20201025_091908_680.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `answers`
+--
+
+CREATE TABLE `answers` (
+  `ans_id` int(11) NOT NULL,
+  `q_id` int(11) NOT NULL,
+  `posted_timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
+  `author` varchar(50) NOT NULL,
+  `answer` text NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `answers`
+--
+
+INSERT INTO `answers` (`ans_id`, `q_id`, `posted_timestamp`, `author`, `answer`, `user_id`) VALUES
+(15, 1, '2021-11-05 15:21:43', 'Arshdeep Singh', 'The eight planets are Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, and Neptune. Mercury is closest to the Sun. Neptune is the farthest. Planets, asteroids, and comets also orbit our Sun', 1),
+(16, 3, '2021-11-05 16:20:13', 'Anonymous', 'Union Territories of India<br>\r\nAndaman and Nicobar Islands.<br>\r\nDadra and Nagar Haveli and Daman and Diu.<br>\r\nChandigarh.<br>\r\nLakshadweep.<br>\r\nPuducherry.<br>\r\nDelhi.<br>\r\nLadakh.<br>\r\nJammu and Kashmir.<br>', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `cat_id` int(11) NOT NULL,
+  `cat_name` varchar(255) NOT NULL,
+  `cat_description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`cat_id`, `cat_name`, `cat_description`) VALUES
+(1, 'General Knowledge', 'This category consists of general questions which are commonly known to many people and are of general origin.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE `questions` (
+  `q_id` int(11) NOT NULL,
+  `Question` text NOT NULL,
+  `author` varchar(100) NOT NULL,
+  `posted_timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
+  `cat_id` int(11) NOT NULL,
+  `status` int(11) DEFAULT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`q_id`, `Question`, `author`, `posted_timestamp`, `cat_id`, `status`, `user_id`) VALUES
+(1, 'What are the 8 planets of the solar system?', 'Anonymous', '2021-11-05 07:22:55', 1, 15, 1),
+(2, 'What is the capital of India?', 'Anonymous', '2021-11-05 16:15:21', 1, NULL, 1),
+(3, 'What are the union territories of India?', 'Arshdeep Singh', '2021-11-05 16:19:09', 1, 16, 1);
 
 --
 -- Indexes for dumped tables
@@ -58,6 +126,24 @@ ALTER TABLE `all_users`
   ADD PRIMARY KEY (`user_id`);
 
 --
+-- Indexes for table `answers`
+--
+ALTER TABLE `answers`
+  ADD PRIMARY KEY (`ans_id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indexes for table `questions`
+--
+ALTER TABLE `questions`
+  ADD PRIMARY KEY (`q_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -65,7 +151,25 @@ ALTER TABLE `all_users`
 -- AUTO_INCREMENT for table `all_users`
 --
 ALTER TABLE `all_users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `answers`
+--
+ALTER TABLE `answers`
+  MODIFY `ans_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `questions`
+--
+ALTER TABLE `questions`
+  MODIFY `q_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
