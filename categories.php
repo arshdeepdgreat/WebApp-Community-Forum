68cb91d89ea0f377b479d91209164f29b8d5b8ee
arshdeepdgreat <arshdeepdgreat@gmail.com>
2021-12-01 21:58:21 +0530

    some changes

--- categories.php
+++ categories.php
@@ -21,7 +21,7 @@
 <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Compose</title>
+    <title>Categories</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <style>
         .phonebox {
@@ -112,6 +112,8 @@
                       <center>
                         <div class="card-content">
                           <h3><?php print_r($cat['cat_name']); ?></h3>
+                          <br>
+                          <h5>Description: <?php print_r($cat['cat_description']);?></h5>
                           <div>
                           <br>
                           <a href="all-q.php?cat_id=<?php echo $cat['cat_id']?>" class="btn brand z-depth-0">View more...</a>
