80fcd312914be712ba1d973dde3ca270c056c766
arshdeepdgreat <arshdeepdgreat@gmail.com>
2021-12-01 21:40:38 +0530

    Yourpanel nav bar fixed

--- yourpanel.php
+++ yourpanel.php
@@ -104,7 +104,7 @@
     <div class="content">
     <nav class="black z-depth-0" style="padding-right: 30px;padding-left: 80px;">
 		<div>
-			<a href="#" class="brand-logo brand-text hide-on-med-and-down">heading</a>
+			<a href="#" class="brand-logo brand-text hide-on-med-and-down">Community Forum</a>
             <ul id="nav-mobile" class="right">
                 <li><a href="browse.php">Browse</a></li>
                 <li><a href="categories.php">Categories</a></li>
